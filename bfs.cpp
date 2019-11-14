4e1ca28008016f8bc91933da6b258377b5243f5f
Wurzelkoch <wurzelkoch@users.sf.net>
2019-11-15 00:56:56 +0100

    Mitten in überlegungen zur Höhenberechnung

---
+++ bfs.cpp
@@ -0,0 +1,33 @@
+// bfs.cpp (Breadth First Search)
+
+#include "graph.h"
+#include "queue.h"
+using namespace std;
+
+int bfs_height(const Graph & graph, Graph::NodeId start_node, vector<vector<int>> &h)
+{
+    std::vector<bool> visited(graph.num_nodes(), false);
+    std::vector<int> dist(graph.num_nodes(), -1);
+    int height = 0;
+    Graph bfs_tree(graph.num_nodes(), graph.dirtype);
+    Queue<Graph::NodeId> queue;
+    queue.push_back(start_node);
+    visited[start_node] = true;
+    dist[start_node] = 0;
+
+    while (not queue.is_empty()) {
+        auto cur_nodeid = queue.pop_front();
+	h[cur_nodeid] =  dist[cur_nodeid];
+	if(dist[cur_nodeid]>height) height = dist[cur_nodeid];
+        for (auto neighbor: graph.get_node(cur_nodeid).adjacent_nodes()) {
+            if (not visited[neighbor.id()]) {
+                visited[neighbor.id()] = true;
+                dist[neighbor.id()] = dist[cur_nodeid] + 1;
+                bfs_tree.add_edge(cur_nodeid, neighbor.id());
+                queue.push_back(neighbor.id());
+            }
+        }
+    }
+    return height;
+}
+
--- rtip.cpp
+++ rtip.cpp
@@ -1,5 +1,6 @@
 #include "graph.h"
 #include "rtip.h"
+#include "bfs.h"
 #include <string>
 
 using namespace std;
@@ -9,10 +10,17 @@ int rtip(Graph g1, Graph::NodeId r1, Graph g2, Graph::NodeId r2)
 {
 	const string alphabet = "0123456789abcdefghijklmnopqrstuvwxyz";
 	vector<string> w1,w2;
-	w1.resize(g1.num_nodes());
+	vector<int> dist_r1(g1.num_nodes(),0), dist_r2(g2.num_nodes(),0);
+	w1.resize(g1.num_nodes()); //vorerst leere Wörter
 	w2.resize(g2.num_nodes());
 	int h1,h2;                 //Berechnung der Höhe fehlt noch
-	vector<int> f1,f2;
+	h1 = bfs_height(g1,r1,dist_r1);
+	h2 = bfs_height(g2,r2,dist_r2);
+	vector<int> f1(g1.num_nodes(),0),f2(g2.num_nodes(),0); //f-Werte
+	
+	for(int h=max(h1,h2);h>=1;h--){
+	}
+
 	int success = 0;
 	return success;
 }
