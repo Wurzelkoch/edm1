f760a16c6ddd1b5dd7daefdd63e1ec59b47985b1
Wurzelkoch <wurzelkoch@users.sf.net>
2019-11-15 10:53:01 +0100

    Läuft jetzt auch

--- bfs.cpp
+++ bfs.cpp
@@ -9,18 +9,25 @@ int bfs_height(const Graph & graph, Graph::NodeId start_node, vector<vector<Grap
     std::vector<bool> visited(graph.num_nodes(), false);
     std::vector<int> dist(graph.num_nodes(), -1);
     int height = 0;
+    h.resize(1);  //Create Bucket for h==0
     Graph bfs_tree(graph.num_nodes(), graph.dirtype);
     Queue<Graph::NodeId> queue;
     queue.push_back(start_node);
     visited[start_node] = true;
     dist[start_node] = 0;
 
+    cout << "starte BFS in " << start_node << " of " << graph.num_nodes() << "\n"; 
+
     while (not queue.is_empty()) {
         auto cur_nodeid = queue.pop_front();
-	if(dist[cur_nodeid]>height){
+		cout << cur_nodeid << " hat Höhe " << dist[cur_nodeid] << "\n";
+	if(dist[cur_nodeid] > height){
+//		cout << "Setting height from " << height << " to " << dist[cur_nodeid] << "\n";
 	    height = dist[cur_nodeid];
-            h.resize(height);
+            h.resize(height+1);
 	}
+//	cout << "Height ist OK\n";
+//	cout << "h has size " << h.size() << " of " << h[dist[cur_nodeid]].size() << "\n";
 	h[dist[cur_nodeid]].push_back(cur_nodeid);
         for (auto neighbor: graph.get_node(cur_nodeid).adjacent_nodes()) {
             if (not visited[neighbor.id()]) {
--- rtip.cpp
+++ rtip.cpp
@@ -13,10 +13,13 @@ int rtip(Graph g1, Graph::NodeId r1, Graph g2, Graph::NodeId r2)
 	vector<vector<Graph::NodeId>> dist_r1, dist_r2;
 	w1.resize(g1.num_nodes()); //vorerst leere Wörter
 	w2.resize(g2.num_nodes());
+	cout << "Bestimme Höhen der Bäume…\n";
 	int h1,h2;                 //Berechnung der Höhe fehlt noch
 	h1 = bfs_height(g1,r1,dist_r1);
+	cout << "Baum 1 hat Höhe " << h1 << "\n";
 	h2 = bfs_height(g2,r2,dist_r2);
-	if(h1 != h2) return 1;
+	cout << "Baum 2 hat Höhe " << h2 << "\n";
+	if(h1 != h2) return 1;     //Bei ungleicher Höhe der Bäume sind sie nicht isomorph.
 	vector<int> f1(g1.num_nodes(),0),f2(g2.num_nodes(),0); //f-Werte
 	
 	for(int h=max(h1,h2);h>=1;h--){
