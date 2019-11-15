be584f734cf01886b4acbd7d7b0408f48bcd4272
Wurzelkoch <wurzelkoch@users.sf.net>
2019-11-15 06:13:17 +0100

    Abbruch bei ungleicher Höhe

--- rtip.cpp
+++ rtip.cpp
@@ -16,6 +16,7 @@ int rtip(Graph g1, Graph::NodeId r1, Graph g2, Graph::NodeId r2)
 	int h1,h2;                 //Berechnung der Höhe fehlt noch
 	h1 = bfs_height(g1,r1,dist_r1);
 	h2 = bfs_height(g2,r2,dist_r2);
+	if(h1 != h2) return 1;
 	vector<int> f1(g1.num_nodes(),0),f2(g2.num_nodes(),0); //f-Werte
 	
 	for(int h=max(h1,h2);h>=1;h--){
