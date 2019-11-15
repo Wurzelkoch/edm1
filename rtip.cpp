9aedf3201224e90dc111b427d080bea121527c19
Wurzelkoch <wurzelkoch@users.sf.net>
2019-11-15 11:37:37 +0100

    Baue Li

--- rtip.cpp
+++ rtip.cpp
@@ -13,6 +13,7 @@ int rtip(Graph g1, Graph::NodeId r1, Graph g2, Graph::NodeId r2)
 	vector<vector<Graph::NodeId>> dist_r1, dist_r2;
 	w1.resize(g1.num_nodes()); //vorerst leere Wörter
 	w2.resize(g2.num_nodes());
+
 	cout << "Bestimme Höhen der Bäume…\n";
 	int h1,h2;                 //Berechnung der Höhe fehlt noch
 	h1 = bfs_height(g1,r1,dist_r1);
@@ -20,9 +21,22 @@ int rtip(Graph g1, Graph::NodeId r1, Graph g2, Graph::NodeId r2)
 	h2 = bfs_height(g2,r2,dist_r2);
 	cout << "Baum 2 hat Höhe " << h2 << "\n";
 	if(h1 != h2) return 1;     //Bei ungleicher Höhe der Bäume sind sie nicht isomorph.
+
 	vector<int> f1(g1.num_nodes(),0),f2(g2.num_nodes(),0); //f-Werte
+	int fmax=0;  //maximaler bisher erreichter f-Wert
 	
 	for(int h=max(h1,h2);h>=1;h--){
+	    vector<Graph::NodeId> L1,L2;
+	    //Sortiere x aus V(Gi) mit Höhe h nach f-Werten:
+	    for(int fi=0;fi<=fmax;fi++){
+                for(auto i=dist_r1[h].begin(); i!=dist_r1[h].end(); i++){
+	            if(f1[*i]==fi) L1.push_back(f1[*i]);
+		}
+                for(auto i=dist_r2[h].begin(); i!=dist_r2[h].end(); i++){
+	            if(f2[*i]==fi) L2.push_back(f2[*i]);
+		}
+	    }
+
 	}
 
 	int success = 0;
